bf1cd655fb9a343dccfd9e6fbd205f69c805f4d8
Maksim Podlesnov <maksimpodlesnov89@gmail.com>
2026-02-26 21:48:49 +0200

    feat: initial commit with project structure and gitignore

---
+++ PropertieForLanguages.DataContext.Sqlite/Migrations/20260214170131_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PropertieForLanguages.DataContext.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Term = table.Column<string>(type: "TEXT", nullable: false),
+                    Translation = table.Column<string>(type: "TEXT", nullable: false),
+                    Language = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Words",
+                columns: new[] { "Id", "Language", "Term", "Translation" },
+                values: new object[,]
+                {
+                    { 1, "en", "Apple", "Яблуко" },
+                    { 2, "en", "Table", "Стіл" },
+                    { 3, "de", "Buch", "Книга" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Words");
+        }
+    }
+}
