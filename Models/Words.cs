90084a165fd895b52692aa71c0e5f7d6110ac32d
Maksim Podlesnov <maksimpodlesnov89@gmail.com>
2026-02-28 15:46:41 +0200

    refactor: fix directory structure and remove duplicate files

--- Models/Words.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PropertieForLanguages.DataContext.Sqlite.Models;
-public class Word
-{
-    public int Id { get; set; }
-    public int GroupId { get; set; }
-    public string Term { get; set; }      // Має збігатися з "Term" у JSON
-    public string Language { get; set; }  // Має збігатися з "Language" у JSON
-}
